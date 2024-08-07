d322fe9d412a059b67400c931e619197070f279e
asimriazasim107@gmail.com <asimriazasim107@gmail.com>
2024-08-07 09:10:30 +0500

    translation of langauge section

---
+++ app/Models/Startpage.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Jenssegers\Mongodb\Eloquent\Model;
+
+
+class Startpage extends Model
+{
+    protected $connection = 'mongodb';
+    use HasFactory;
+
+    protected $fillable = [
+        // 'language_id',
+        'language',
+        'our_policy',
+        'login',
+        'sign_up',
+        'dear_guest ',
+        'create_account'
+    ];
+
+
+    
+}
