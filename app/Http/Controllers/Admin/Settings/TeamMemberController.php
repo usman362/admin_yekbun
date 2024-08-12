2bf0f645566ec95fbf0e8fafb1a7e8d31a80ca64
usman362 <usmanshoaib362@gmail.com>
2024-08-12 06:46:48 +0500

    changes

--- app/Http/Controllers/Admin/Settings/TeamMemberController.php
+++ app/Http/Controllers/Admin/Settings/TeamMemberController.php
@@ -25,7 +25,7 @@ class TeamMemberController extends Controller
      */
     public function index()
     {
-        $users = User::where('status', 1)->where('is_admin_user', 0);
+        $users = User::where('status', 1)->where('is_admin_user', 1);
         $users = $users->with('roles')->get();
         $roles = Role::all();
         return view("content.settings.team_members.index", compact("users", "roles"));
