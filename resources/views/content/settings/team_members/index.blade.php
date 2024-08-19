a85698e7237107c96fd4e3fedfb02d0248b62e2d
usman362 <usmanshoaib362@gmail.com>
2024-08-19 15:22:26 +0500

    changes

--- resources/views/content/settings/team_members/index.blade.php
+++ resources/views/content/settings/team_members/index.blade.php
@@ -61,7 +61,7 @@
             <td>
               <div class="d-flex justify-content-start align-items-center user-name">
                 <div class="avatar-wrapper">
-                  <div class="avatar avatar-sm me-3"><img src="{{$user->image? url('storage/' . $user->image): 'https://www.w3schools.com/howto/img_avatar.png' }}" alt="Avatar" class="rounded-circle"></div>
+                  <div class="avatar avatar-sm me-3"><img src="{{$user->image? asset('storage/' . $user->image): 'https://www.w3schools.com/howto/img_avatar.png' }}" alt="Avatar" class="rounded-circle"></div>
                 </div>
                 <div class="d-flex flex-column">
                   <a href="javascript:void(0)" class="text-body text-truncate">
