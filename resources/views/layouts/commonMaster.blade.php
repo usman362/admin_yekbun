600bf5445accedf6022c18587bc498a0b8f39b29
usman362 <usmanshoaib362@gmail.com>
2024-11-07 15:53:34 +0500

    login timer session

--- resources/views/layouts/commonMaster.blade.php
+++ resources/views/layouts/commonMaster.blade.php
@@ -47,13 +47,69 @@
   @include('layouts/sections/scripts')
 
   <script>
-    @if(session('success'))
-      toastr.success('{{ session('success') }}');
-    @endif
-    @if(session('error'))
-      toastr.error('{{ session('error') }}');
-    @endif
-  </script>
+
+        // Inactivity Timer Script
+        let inactivityTime = function () {
+          let time;
+          let maxInactivity = 120 * 60 * 1000; // 2 hours
+
+          window.onload = resetTimer;
+          document.onmousemove = resetTimer;
+          document.onkeypress = resetTimer;
+
+          function showLogoutModal() {
+            console.log("Inactivity detected. Showing modal.");
+            // Display the modal
+            $('#inactivityModal').modal('show');
+          }
+
+          function resetTimer() {
+            console.log("Activity detected. Resetting timer.");
+            clearTimeout(time);
+            time = setTimeout(showLogoutModal, maxInactivity);
+          }
+        };
+
+        window.onload = function () {
+          inactivityTime();
+        };
+
+        function stayLoggedIn() {
+          console.log("Stay logged in clicked.");
+          $('#inactivityModal').modal('hide');
+          inactivityTime();
+          location.reload();
+        }
+
+        function submitLogoutForm() {
+          console.log("Logout clicked.");
+          document.getElementById('logout-form').submit();
+        }
+      </script>
+
+      <!-- Inactivity Modal -->
+      <div class="modal fade" id="inactivityModal" tabindex="-1" role="dialog" aria-labelledby="inactivityModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title text-center" id="inactivityModalLabel">Your Session is Expired</h5>
+            </div>
+            <div class="modal-body text-center">
+              <p>Do you need more time?</p>
+              <p>You will be logged out soon due to inactivity.</p>
+            </div>
+            <div class="modal-footer justify-content-center">
+              <button type="button" class="btn btn-primary" onclick="stayLoggedIn()">Stay Online</button>
+              <a class="btn btn-secondary" href="{{ route('admin.logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                Logout
+              </a>
+              <form id="logout-form" action="{{ route('admin.logout') }}" method="POST" class="d-none">
+                @csrf
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
 {{--
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script> --}}
