f24643ca08386c4b1a8c13bd73053ebda0cc6b52
Muhammad Ali Asghar <alidevelopement01@gmail.com>
2025-03-13 02:34:17 +0500

    ch

--- config/laravel-ffmpeg.php
+++ config/laravel-ffmpeg.php
@@ -2,13 +2,13 @@
 
 return [
     'ffmpeg' => [
-        'binaries' => env('FFMPEG_BINARIES', 'ffmpeg'),
+        'binaries' => '/usr/bin/ffmpeg',
 
         'threads' => 12,   // set to false to disable the default 'threads' filter
     ],
 
     'ffprobe' => [
-        'binaries' => env('FFPROBE_BINARIES', 'ffprobe'),
+        'binaries' => '/usr/bin/ffprobe',
     ],
 
     'timeout' => 3600,
