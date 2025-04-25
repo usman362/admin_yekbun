49cf5ff7ac0c0e6d8fc75b685bfb405ec3960605
Muhammad Usman <120487584+alidevelopement012@users.noreply.github.com>
2025-04-26 00:54:50 +0500

    changes

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -354,4 +354,65 @@ class UsersController extends Controller
             ]);
         }
     }
+
+    public function testNotification(Request $request)
+    {
+
+        $user = \App\Models\User::find($request->user_id);
+        $fcm = $user->fcm_token;
+
+        if (!$fcm) {
+            return response()->json(['message' => 'User does not have a device token'], 400);
+        }
+
+        $projectId = env('FCM_PROJECT_ID'); # INSERT COPIED PROJECT ID
+
+        $credentialsFilePath = Storage::path('json/services.json');
+        $client = new GoogleClient();
+        $client->setAuthConfig($credentialsFilePath);
+        $client->addScope('https://www.googleapis.com/auth/firebase.messaging');
+        $client->refreshTokenWithAssertion();
+        $token = $client->getAccessToken();
+
+        $access_token = $token['access_token'];
+
+        $headers = [
+            "Authorization: Bearer $access_token",
+            'Content-Type: application/json'
+        ];
+
+        $data = [
+            "message" => [
+                "token" => $fcm,
+                "notification" => [
+                    "title" => $request->title,
+                    "body" => $request->body,
+                ],
+            ]
+        ];
+        $payload = json_encode($data);
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://fcm.googleapis.com/v1/projects/{$projectId}/messages:send");
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+        curl_setopt($ch, CURLOPT_VERBOSE, true); // Enable verbose output for debugging
+        $response = curl_exec($ch);
+        $err = curl_error($ch);
+        curl_close($ch);
+
+        if ($err) {
+            return response()->json([
+                'message' => 'Curl Error: ' . $err
+            ], 500);
+        } else {
+            return response()->json([
+                'message' => 'Notification has been sent',
+                'response' => json_decode($response, true)
+            ]);
+        }
+    }
 }
