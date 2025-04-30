8e9ea8759932a76b0876e15953d14dacf74aa117
Muhammad Usman <120487584+alidevelopement012@users.noreply.github.com>
2025-04-30 16:29:36 +0500

    edit artist modal issue fixed

--- resources/views/content/include/artist/editForm.blade.php
+++ resources/views/content/include/artist/editForm.blade.php
@@ -10,74 +10,73 @@
 </style>
 
 
-<form id="editForm{{ $artist->id }}" class="edit-form" method="post" action="{{ route('artist.update', $artist->id) }}" enctype="multipart/form-data">
+<form id="editForm{{ $artist->id }}" class="edit-form" method="post" action="{{ route('artist.update', $artist->id) }}"
+    enctype="multipart/form-data">
     @csrf
     @method('PUT')
     <div class="hidden-inputs">
         <input type="hidden" name="image" value="{{ $artist->image }}" data-path="{{ $artist->image }}">
     </div>
     <div class="row">
-        <div class="col-lg-12 mx-auto">
 
-            <div class="row g-3">
 
-                <div class="col-md-6">
-                    <label class="form-label" for="fullname">Gender</label>
-                    <select name="gender" class="form-select">
-                        <option>Select Gender</option>
-                        @if ($artist->gender == 'male')
-                            <option value="male" selected>Male</option>
-                            <option value="female">Female</option>
-                        @else
-                            <option value="female" selected>Female</option>
-                            <option value="male">Male</option>
-                        @endif
-                    </select>
-                </div>
+        <div class="col-md-6">
+            <label class="form-label" for="fullname">Gender</label>
+            <select name="gender" class="form-select">
+                <option>Select Gender</option>
+                @if ($artist->gender == 'male')
+                    <option value="male" selected>Male</option>
+                    <option value="female">Female</option>
+                @else
+                    <option value="female" selected>Female</option>
+                    <option value="male">Male</option>
+                @endif
+            </select>
+        </div>
 
-                <div class="col-md-6">
-                    <label class="form-label" for="fullname">Province</label>
-                    <select name="province" class="form-control province_id" id="province_id" data-url={{ url('get/city') }} value="{{ old('province_id') }}" data-selected="{{ $artist->city_id }}">
-                        <option value="">Select province</option>
-                        @foreach($provinces as $province)
-                        <option value="{{ $province->id }}" {{ $province->id == $artist->province_id? 'selected': '' }}>{{ $province->name ?? '' }}</option>
-                        @endforeach
-                    </select>
-                </div>
+        <div class="col-md-6">
+            <label class="form-label" for="fullname">Province</label>
+            <select name="province" class="form-control province_id" id="province_id" data-url={{ url('get/city') }}
+                value="{{ old('province_id') }}" data-selected="{{ $artist->city_id }}">
+                <option value="">Select province</option>
+                @foreach ($provinces as $province)
+                    <option value="{{ $province->id }}" {{ $province->id == $artist->province_id ? 'selected' : '' }}>
+                        {{ $province->name ?? '' }}</option>
+                @endforeach
+            </select>
+        </div>
 
-                <div class="col-md-12">
-                    <label class="form-label" for="fullname">Artist Name - Lastname</label>
-                    <input type="text" id="fullname" class="form-control" placeholder="lorem" name="name"
-                        value="{{ $artist->name ?? '' }}">
-                </div>
+        <div class="col-md-12 mb-2">
+            <label class="form-label" for="fullname">Artist Name - Lastname</label>
+            <input type="text" id="fullname" class="form-control" placeholder="lorem" name="name"
+                value="{{ $artist->name ?? '' }}">
+        </div>
 
-                <div class="col-12">
-                    <div class="card">
-                        <h5 class="card-header">Image</h5>
-                        <div class="card-body">
-                            <div class="dropzone needsclick" action="/" id="dropzone-img{{ $artist->id }}">
-                                <div class="dz-message needsclick">
-                                    Drop files here or click to upload
-                                </div>
-                                <div class="fallback">
-                                    <input type="file" name="image" />
-                                </div>
-                            </div>
+        <div class="col-12">
+            <div class="card">
+                <h5 class="card-header">Image</h5>
+                <div class="card-body">
+                    <div class="dropzone needsclick" action="/" id="dropzone-img{{ $artist->id }}">
+                        <div class="dz-message needsclick">
+                            Drop files here or click to upload
+                        </div>
+                        <div class="fallback">
+                            <input type="file" name="image" />
                         </div>
                     </div>
                 </div>
-
-                <div class="col-md-12">
-                    <label class="form-label" for="status">Status</label>
-                    <select class="form-select" name="status">
-                        <option selected value="">Select</option>
-                        <option  value="1" {{ $artist->status == 1? 'selected': '' }}>Publish</option>
-                        <option  value="0" {{ $artist->status == 0? 'selected': '' }}>UnPublish</option>
-
-                    </select>
-
-                </div>
             </div>
+        </div>
+
+        <div class="col-md-12">
+            <label class="form-label" for="status">Status</label>
+            <select class="form-select" name="status">
+                <option selected value="">Select</option>
+                <option value="1" {{ $artist->status == 1 ? 'selected' : '' }}>Publish</option>
+                <option value="0" {{ $artist->status == 0 ? 'selected' : '' }}>UnPublish</option>
+
+            </select>
+
         </div>
     </div>
 </form>
@@ -127,12 +126,16 @@
                 success: function(file, response) {
                     file.previewElement.classList.add("dz-success");
                     file.previewElement.dataset.path = response.path;
-                    const hiddenInputsContainer = dropzoneElement.closest('.dropzone-container').querySelector('.hidden-inputs');
-                    hiddenInputsContainer.innerHTML += `<input type="hidden" name="image" value="${response.path}" data-path="${response.path}">`;
+                    const hiddenInputsContainer = dropzoneElement.closest('.dropzone-container')
+                        .querySelector('.hidden-inputs');
+                    hiddenInputsContainer.innerHTML +=
+                        `<input type="hidden" name="image" value="${response.path}" data-path="${response.path}">`;
                 },
                 removedfile: function(file) {
-                    const hiddenInputsContainer = dropzoneElement.closest('.dropzone-container').querySelector('.hidden-inputs');
-                    hiddenInputsContainer.querySelector(`input[data-path="${file.previewElement.dataset.path}"]`).remove();
+                    const hiddenInputsContainer = dropzoneElement.closest('.dropzone-container')
+                        .querySelector('.hidden-inputs');
+                    hiddenInputsContainer.querySelector(
+                        `input[data-path="${file.previewElement.dataset.path}"]`).remove();
 
                     file.previewElement.parentNode.removeChild(file.previewElement);
 
@@ -142,13 +145,15 @@
                         headers: {
                             'X-CSRF-TOKEN': '{{ csrf_token() }}'
                         },
-                        data: { path: file.previewElement.dataset.path },
+                        data: {
+                            path: file.previewElement.dataset.path
+                        },
                         success: function() {}
                     });
 
                     return this._updateMaxFilesReachedClass();
                 }
-                
+
             });
         });
     }
@@ -163,7 +168,9 @@
         const blob = await response.blob();
 
         // Create a File object
-        const file = new File([blob], fileName[1], { type: blob.type });
+        const file = new File([blob], fileName[1], {
+            type: blob.type
+        });
 
         return file;
     }
