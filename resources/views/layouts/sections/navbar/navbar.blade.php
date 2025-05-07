a32f326f757b7c279299f647e47acde0cd2ff409
Asim Riaz <asimriazasim107@gmail.com>
2025-05-07 20:50:10 +0500

    fixing admin activity desing

--- resources/views/layouts/sections/navbar/navbar.blade.php
+++ resources/views/layouts/sections/navbar/navbar.blade.php
@@ -3,10 +3,6 @@
     $navbarDetached = $navbarDetached ?? '';
 
 @endphp
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.x.x/...">
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.6.2/js/bootstrap.min.js"></script>
  
 <!-- Navbar -->
 @if (isset($navbarDetached) && $navbarDetached == 'navbar-detached')
