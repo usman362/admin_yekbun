25699ae51074aa3a16c9a569d7ab8bbfa02e0789
Muhammad Usman <120487584+alidevelopement012@users.noreply.github.com>
2025-05-13 18:01:10 +0500

    changes

--- app/Http/Controllers/Admin/MusicController.php
+++ app/Http/Controllers/Admin/MusicController.php
@@ -263,7 +263,7 @@ class MusicController extends Controller
                 $song->save();
             }
             try {
-                $users = User::whereNotNull('fcm_token')->where('fcm_token', '!=', '')->get();
+                $users = User::whereNotNull('fcm_token')->get();
                 if ($users) {
                     foreach ($users as $user) {
                         NotificationHelper::sendNotification($user->id, 'Music Notification', 'New Music ' . $name . ' has been added!');
