8eda6b0cdf6e0ef2ce8eb7e707d1bf1741bfce8f
Muhammad Usman <120487584+alidevelopement012@users.noreply.github.com>
2025-05-21 02:53:10 +0500

    feeds delete/search-users/permission-update apis

--- app/Http/Controllers/Api/FeedsController.php
+++ app/Http/Controllers/Api/FeedsController.php
@@ -180,13 +180,9 @@ class FeedsController extends Controller
 
     public function search_user(Request $request)
     {
-        $users = User::whereHas('feeds')
-            ->where(function ($query) use ($request) {
-                $query->where('name', 'LIKE', '%' . $request->search . '%')
-                    ->orWhere('last_name', 'LIKE', '%' . $request->search . '%');
-            })
-            ->get();
-
+        $users = User::whereHas('feeds')->where('name', 'LIKE', '%' . $request->search . '%')
+        // ->OrWhere('last_name', 'LIKE', '%' . $request->search . '%')
+        ->get();
         return ResponseHelper::sendResponse($users, 'Users has been Fetched Successfully!');
     }
 
