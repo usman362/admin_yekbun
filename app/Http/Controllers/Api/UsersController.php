0c00360f98e6baf3902e8f62f3d6f9ff756fd551
Muhammad Usman <120487584+alidevelopement012@users.noreply.github.com>
2025-05-20 15:25:04 +0500

    changes

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -184,7 +184,7 @@ class UsersController extends Controller
                 $q->with('user');
             }])->find($id);
             $friends_list = $user->friends ?? [];
-            return ResponseHelper::sendResponse(['friends_list' => $friends_list, 'family_list' => $family_list], 'Friends List Fetch Successfully');
+            return ResponseHelper::sendResponse(['user' => User::find($id),'friends_list' => $friends_list, 'family_list' => $family_list], 'Friends List Fetch Successfully');
             // }
         } catch (Exception $e) {
             return ResponseHelper::sendResponse([], 'Error to Fetch Friends List', false, 403);
