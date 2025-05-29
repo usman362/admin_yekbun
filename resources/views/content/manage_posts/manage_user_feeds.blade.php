493e5230c9ac45a4ec0b66026adc467e13cf1acb
Asim Riaz <asimriazasim107@gmail.com>
2025-05-29 12:08:03 +0500

    testing something for feed

--- resources/views/content/manage_posts/manage_user_feeds.blade.php
+++ resources/views/content/manage_posts/manage_user_feeds.blade.php
@@ -668,6 +668,7 @@
                                             <div class="d-flex justify-content-between align-items-center">
                                                 <div class="d-flex align-items-center">
                                                     <button class="btn btn-white p-3">ID</button>
+                                                    <button class="btn btn-white p-3">{{ optional($feed->user)->user_id }}</button>
                                                 </div>
                                                 <div class="d-flex align-items-center" style="gap: 7px;">
                                                     <button class="btn btn-white_01 p-3">11.10.2025</button>
@@ -675,10 +676,9 @@
                                                 </div>
                                             </div>
                                             <p class="mb-0 mt-2 p-1"
-                                                style="    font-size: 14px;
-    background: #fff; border-radius: 4px;">
+                                                style="font-size: 14px;background: #fff; border-radius: 4px;">
                                                 Reason:
-                                                ipsum dolor sit amet, consectetur adipisicing.</p>
+                                               {{ $feed->report_type }}</p>
                                         </div>
                                     </div>
                                     <div class="nav-item dropdown d-block"
@@ -876,6 +876,7 @@
                                             <div class="d-flex justify-content-between align-items-center">
                                                 <div class="d-flex align-items-center">
                                                     <button class="btn btn-white p-3">ID</button>
+                                                    <button class="btn btn-white p-3">{{  }}</button>
                                                 </div>
                                                 <div class="d-flex align-items-center" style="gap: 7px;">
                                                     <button class="btn btn-white_01 p-3">11.10.2025</button>
