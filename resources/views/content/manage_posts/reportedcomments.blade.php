a1e16a90719fa91c6f715622d7a110bec4b87bf0
Asim Riaz <asimriazasim107@gmail.com>
2025-05-30 12:14:19 +0500

    added something for dashbard

--- resources/views/content/manage_posts/reportedcomments.blade.php
+++ resources/views/content/manage_posts/reportedcomments.blade.php
@@ -448,7 +448,7 @@
                                             </div>
                                             <p class="mb-0 mt-2 p-1"
                                                 style="font-size: 14px;background: #fff; border-radius: 4px;">
-                                                Reason: {{ $feeds->report_type }}</p>
+                                                Reason:  </p>
                                         </div>
                                     </div>
                                     <div class="nav-item dropdown d-block"
