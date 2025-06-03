a73a8a3bcd3842928e70d500f48eedcf1b7733c8
Asim Riaz <asimriazasim107@gmail.com>
2025-06-03 09:08:18 +0500

    aded

--- resources/views/content/manage_posts/manage_user_feeds.blade.php
+++ resources/views/content/manage_posts/manage_user_feeds.blade.php
@@ -721,10 +721,10 @@
                                                         ? $reportcomments->users->image
                                                         : asset('storage/' . $reportcomments->users->image))
                                                     : 'https://www.w3schools.com/w3images/avatar2.png' }}"
-                                                    style="width: 25px !important; height: 25px !important; border-radius: 4px !important; margin: 9px 6px;">
+                                                    style="width: 25px !important; height: 25px !important;  border-radius: 4px !important; margin: 9px 6px;">
                                                 <div class="w-100 d-flex flex-column">
                                                        <p class="mb-0 mt-2 p-1"
-                                                style="font-size: 14px;  border-radius: 4px;">
+                                                style="font-size: 14px;  border-radius: 4px; background: #fff; ">
                                                 <span style="font-weight: bold;">User name</span>
 
                                                 @if ($reportcomments->comments)
