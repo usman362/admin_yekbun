d50c052e741649e36d7ab00d1284d69feb155f6c
Asim Riaz <asimriazasim107@gmail.com>
2025-06-04 23:42:45 +0500

    aded somhting

--- resources/views/content/manage_posts/manage_user_feeds.blade.php
+++ resources/views/content/manage_posts/manage_user_feeds.blade.php
@@ -827,10 +827,7 @@
      <div class="modal fade" id="editFeedModal" tabindex="-1" aria-labelledby="editFeedModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" style="max-width: 400px;">
         <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="editFeedModalLabel">View Feed</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
+            
             <div class="modal-body" id="editFeedContent">
                 <!-- Feed content will be injected here -->
             </div>
