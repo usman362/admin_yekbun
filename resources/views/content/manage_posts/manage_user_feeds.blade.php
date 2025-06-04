f4062cfa10552072915c2dadd4b3f4c48b9994e8
Asim Riaz <asimriazasim107@gmail.com>
2025-06-04 23:11:30 +0500

    aded somhting

--- resources/views/content/manage_posts/manage_user_feeds.blade.php
+++ resources/views/content/manage_posts/manage_user_feeds.blade.php
@@ -828,7 +828,7 @@
             <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="editFeedModalLabel">Edit Feed</h5>
+                        <h5 class="modal-title" id="editFeedModalLabel">View  Feed</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body" id="editFeedContent">
