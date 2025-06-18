21f004ced2a2feef5c93c81f1e9ab0abca5384db
Asim Riaz <asimriazasim107@gmail.com>
2025-06-18 22:44:10 +0500

    adde

--- resources/views/content/bank-transfer/index.blade.php
+++ resources/views/content/bank-transfer/index.blade.php
@@ -201,7 +201,7 @@
 
     <div class="d-flex align-items-center">
         <div class="d-flex align-items-center ms-3" style="border-radius: 10px;">
-            <div class="input-group time_input" style="width: 200px;">
+            <div class="input-group time_input" style="width: 260px;">
                 <input type="text" class="form-control time_input_field datepicker" style="height: 27px;margin-top: 7px;"
                     placeholder="Select Date" name="duration" id="datepicker"
                     aria-label="Datepicker 1" autocomplete="off" />
