4a3c9377ff5e7258275e9071f8d37666a04f13eb
Asim Riaz <asimriazasim107@gmail.com>
2025-06-18 15:05:31 +0500

    added

--- resources/views/content/bank-transfer/index.blade.php
+++ resources/views/content/bank-transfer/index.blade.php
@@ -24,86 +24,236 @@
     overflow: hidden;
   }
 </style>
+ <style>
+        .card,
+        .modal {
+            font-family: 'Genos';
+        }
+
+        .transaction-icons img {
+            width: 100%;
+            height: 50px;
+            margin: 0 auto;
+        }
+
+        .transaction-icons h4 {
+            margin: 0;
+            font-size: 20px;
+            text-align: center;
+        }
+
+        .transaction-icons p {
+            font-size: 18px;
+            text-align: center;
+            margin: 0;
+        }
+
+        .transaction-icons i {
+            font-size: 14px;
+        }
+
+        .transaction-icons {
+            padding: 12px 38px 0px 38px;
+            width: 100%;
+            border-right: 2px solid #F2F2F2;
+        }
+
+        .transaction-icons:last-child {
+            border-right: none !important;
+        }
+
+        .nav-pills .nav-link {
+            font-size: 16px !important;
+        }
+
+        .nav-pills .nav-link.active,
+        .nav-pills .nav-link.active:hover,
+        .nav-pills .nav-link.active:focus {
+            background-color: transparent !important;
+            color: #000 !important;
+            font-weight: bold !important;
+            box-shadow: none !important;
+        }
+
+        .line-height-1 {
+            line-height: 1 !important;
+        }
+
+        .user-area span {
+            font-size: 14px;
+        }
+
+        .user-area img {
+            width: 10px;
+            height: 10px;
+            margin: 4px 2px 0 2px;
+        }
+
+        .user-area .user-avatar {
+            width: 28px;
+            height: 28px;
+            border-radius: 30px;
+        }
+
+        .user-area span {
+            font-size: 12px;
+            font-weight: 500;
+            margin-top: 2px;
+        }
+    </style>
 @endsection
 
 @section('content')
 <!-- Basic Bootstrap Table -->
-<div class="card">
+
+ <div class="card mb-4"> <!-- First Card: Bank Transfer List -->
     <div class="card-header d-flex align-items-center justify-content-between">
-        <h5 class="m-0">Bank Transfer List</h5>
-        <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#createbankModal"><i class="bx bx-plus me-0 me-sm-1"></i> Add Bank</button>
+        <h5 class="m-0"><strong>Bank Transfer List</strong></h5>
+        <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#createbankModal">
+            <i class="bx bx-plus me-0 me-sm-1"></i> Add Bank
+        </button>
     </div>
-       @if(session('success'))
+
+    @if(session('success'))
     <div class="alert alert-success alert-dismissible fade show" role="alert">
         {{ session('success') }}
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
-@endif
-@if(session('error'))
+    @endif
+    @if(session('error'))
     <div class="alert alert-danger alert-dismissible fade show" role="alert">
         {{ session('error') }}
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
-@endif
-    <div class="table-responsive text-nowrap">
-      <table class="table">
-        <thead>
-          <tr>
-            <th>Id</th>
-            <th>Title</th>
-            <th>Bank Name</th>
-            <th>Owner Name </th>
-            <th>Account No</th>
-            <th>IBAN No</th>
-            <th>Option</th>
-          </tr>
-        </thead>
-        <tbody class="table-border-bottom-0">
-        @forelse($banks as $bank)
-        <tr>
-          <td>{{ $loop->iteration }}</td>
-          <td>{{ $bank->add_title ?? '' }}</td>
-          <td>{{ $bank->bank_name ?? '' }}</td>
-          <td>{{ $bank->account_holder_name ?? '' }}</td>
-          <td>{{ $bank->account_no ?? '' }}</td>
-          <td>{{ $bank->iban_no ?? '' }}</td>
-          <td>
-              <div class="dropdown d-inline-block">
-                <!-- Edit -->
-                <span data-bs-toggle="modal" data-bs-toggle="modal" data-bs-target="#editbankModal{{ $bank->id }}">
-                  <button class="btn btn-sm btn-icon" data-bs-toggle="tooltip" data-bs-offset="0,4" data-bs-placement="top" data-bs-html="true" data-bs-original-title="Edit">
-                    <i class="bx bx-edit"></i>
-                  </button>
-                </span>
+    @endif
 
-                <!-- Delete -->
-                <form action="{{ route('settings.bank-transfer.destroy',$bank->id) }}" onsubmit="confirmAction(event, () => event.target.submit())" method="post" class="d-inline">
-                  @method('DELETE')
-                  @csrf
-                  <button type="submit" class="btn btn-sm btn-icon" data-bs-toggle="tooltip" data-bs-offset="0,4" data-bs-placement="top" data-bs-html="true" data-bs-original-title="Remove"><i class="bx bx-trash me-1"></i></button>
-                </form>
-           
-              </div>
-              <x-modal id="editbankModal{{ $bank->id }}" 
-                  title="Edit Bank "
-                  saveBtnText="Update" 
-                  saveBtnType="submit"
-                saveBtnForm="editForm{{ $bank->id }}" 
-                    size="md">
-                @include('content.bank-transfer.editForm')
-              </x-modal>
-            </td>
-        </tr>
-        @empty
-        <tr>
-            <td class="text-center" colspan="8"><b>No Bank  found.<b></td>
-        </tr>
-        @endforelse
-        </tbody>
-      </table>
+    <div class="card-body">
+        <div class="table-responsive text-nowrap">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Title</th>
+                        <th>Bank Name</th>
+                        <th>Owner Name</th>
+                        <th>Account No</th>
+                        <th>IBAN No</th>
+                        <th>Option</th>
+                    </tr>
+                </thead>
+                <tbody class="table-border-bottom-0">
+                    @forelse($banks as $bank)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $bank->add_title ?? '' }}</td>
+                        <td>{{ $bank->bank_name ?? '' }}</td>
+                        <td>{{ $bank->account_holder_name ?? '' }}</td>
+                        <td>{{ $bank->account_no ?? '' }}</td>
+                        <td>{{ $bank->iban_no ?? '' }}</td>
+                        <td>
+                            <div class="dropdown d-inline-block">
+                                <!-- Edit -->
+                                <span data-bs-toggle="modal" data-bs-target="#editbankModal{{ $bank->id }}">
+                                    <button class="btn btn-sm btn-icon" data-bs-toggle="tooltip" data-bs-offset="0,4" data-bs-placement="top" data-bs-html="true" data-bs-original-title="Edit">
+                                        <i class="bx bx-edit"></i>
+                                    </button>
+                                </span>
+
+                                <!-- Delete -->
+                                <form action="{{ route('settings.bank-transfer.destroy',$bank->id) }}" onsubmit="confirmAction(event, () => event.target.submit())" method="post" class="d-inline">
+                                    @method('DELETE')
+                                    @csrf
+                                    <button type="submit" class="btn btn-sm btn-icon" data-bs-toggle="tooltip" data-bs-offset="0,4" data-bs-placement="top" data-bs-html="true" data-bs-original-title="Remove"><i class="bx bx-trash me-1"></i></button>
+                                </form>
+                            </div>
+                            <x-modal id="editbankModal{{ $bank->id }}" 
+                                title="Edit Bank "
+                                saveBtnText="Update" 
+                                saveBtnType="submit"
+                                saveBtnForm="editForm{{ $bank->id }}" 
+                                size="md">
+                            @include('content.bank-transfer.editForm')
+                            </x-modal>
+                        </td>
+                    </tr>
+                    @empty
+                    <tr>
+                        <td class="text-center" colspan="7"><b>No Bank found.<b></td>
+                    </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
     </div>
-  </div>
-  <!--/ Basic Bootstrap Table -->
+</div>
+
+<!-- Space between the two cards -->
+<div style="height: 30px;"></div> <!-- This will create a clear gap between the cards -->
+
+<!-- Second Card: Transaction List -->
+<div class="card">
+    <div class="card-header">
+        <h5 class="m-0"><strong>Transactions Bank Overview</strong></h5>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive text-nowrap">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Order</th>
+                        <th>Date & Time</th>
+                        <th>About User</th>
+                        <th>Service Type</th>
+                        <th>Payment Type</th>
+                        <th>Total Paid</th>
+                        <th>Options</th>
+                    </tr>
+                </thead>
+                <tbody class="table-border-bottom-0">
+                    <tr>
+                        <td>01</td>
+                        <td>Order ID</td>
+                        <td class="line-height-1">
+                            <p class="m-0"><strong>DD.MM.YYYY</strong></p>
+                            <small>HH:MM</small>
+                        </td>
+                        <td class="line-height-1 user-area">
+                            <div class="d-flex">
+                                <img class="user-avatar" src="{{ asset('images/user-clips-artist.png') }}" alt="">
+                                <div>
+                                    <h5 class="m-0"><strong>Username</strong></h5>
+                                    <div class="d-flex">
+                                        <img src="{{ asset('images/kurdistan-flag-sm.png') }}" alt="">
+                                        <span>Rojava . Qamishlo</span>
+                                        <img src="{{ asset('images/germany-flag-sm.png') }}" alt="">
+                                        <span>Hannover</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                        <td class="line-height-1">
+                            <p class="m-0"><strong>Upgrades</strong></p>
+                            <small>Educated</small>
+                        </td>
+                        <td class="line-height-1">
+                            <p class="m-0"><strong>Paypal</strong></p>
+                            <small>Transaction ID</small>
+                        </td>
+                        <td class="line-height-1">
+                            <p class="m-0"><strong>15,00€</strong></p>
+                            <small>-0% Discount</small>
+                        </td>
+                        <td><a href="javascript:void(0)"><img class="w-80" src="{{ asset('assets/svg/eye.svg') }}" alt="view"></a></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+
+   <!--/ Basic Bootstrap Table -->
   <x-modal id="createbankModal" title="Create Bank" saveBtnText="Create" saveBtnType="submit" saveBtnForm="createForm" size="md">
     @include('content.include.bank-transfer.createForm')
 </x-modal>
