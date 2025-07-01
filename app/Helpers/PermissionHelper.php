120a6ab1c9014272fdf286077e9692963e371cdd
Muhammad Usman <120487584+alidevelopement012@users.noreply.github.com>
2025-07-01 22:25:10 +0500

    cchages

--- app/Helpers/PermissionHelper.php
+++ app/Helpers/PermissionHelper.php
@@ -14,6 +14,7 @@ class PermissionHelper
             2 => 'academic',
         ];
         $permissions = Setting::where('name', $userType[$level])->first();
+        dd($permissions);
         $test = $permissions['value'][$key];
 
         if ($test === true) {
