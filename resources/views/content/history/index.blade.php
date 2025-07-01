35a3ceb453d4891e44c3b27f17110cf75af13b6f
Asim Riaz <asimriazasim107@gmail.com>
2025-07-01 09:55:28 +0500

    added

--- resources/views/content/history/index.blade.php
+++ resources/views/content/history/index.blade.php
@@ -304,9 +304,10 @@
                                                     <div class="user-info">
                                                         <div class="row">
                                                             <div class="col-md-2 p-0">
-                                                                {{-- <img src="{{ asset('storage/' . (optional($feed->user)->image ?? '')) }}"
-                                                                    style="width: 100px !important;height:36px !important;"
-                                                                    onerror="this.src='https://www.w3schools.com/w3images/avatar2.png'"> --}}
+                                                                <img src="{{ asset('storage/' . (optional($feed->user)->image ?? '')) }}"
+     style="width: 100px !important; height: 36px !important; visibility: hidden;"
+     onerror="this.style.visibility='hidden'">
+
                                                             </div>
                                                             <div class="col-md-10">
                                                                 <p class="m-0" title="{{ $feed->title }}">
