452908672cfafc729f44aeb2761e100b08dd9a14
Asim Riaz <asimriazasim107@gmail.com>
2025-07-01 21:29:44 +0500

    addd

--- app/Helpers/PermissionHelper.php
+++ app/Helpers/PermissionHelper.php
@@ -6,54 +6,22 @@ use App\Models\Setting;
 
 class PermissionHelper
 {
-    // public static function checkPermission($level, $key, $message = null)
-    // {
-    //     $userType = [
-    //         0 => 'cultivated',
-    //         1 => 'educated',
-    //         2 => 'academic',
-    //     ];
-    //     $permissions = Setting::where('name', $userType[$level])->first();
-    //     $test = $permissions['value'][$key];
-
-    //     if ($test === true) {
-    //         return true;
-    //     } elseif ($test !== true && $test !== false) {
-    //         return $test;
-    //     } else {
-    //         return false;
-    //     }
-    // }
     public static function checkPermission($level, $key, $message = null)
-{
-    $userType = [
-        0 => 'cultivated',
-        1 => 'educated',
-        2 => 'academic',
-    ];
+    {
+        $userType = [
+            0 => 'cultivated',
+            1 => 'educated',
+            2 => 'academic',
+        ];
+        $permissions = Setting::where('name', $userType[$level])->first();
+        $test = $permissions['value'][$key];
 
-    // Validate user type
-    if (!array_key_exists($level, $userType)) {
-        return false;
+        if ($test === true) {
+            return true;
+        } elseif ($test !== true && $test !== false) {
+            return $test;
+        } else {
+            return false;
+        }
     }
-
-    // Fetch settings for user type
-    $permissions = Setting::where('name', $userType[$level])->first();
-
-    // Ensure permissions exist and value is an array
-    if (!$permissions || !is_array($permissions->value) || !array_key_exists($key, $permissions->value)) {
-        return false;
-    }
-
-    $test = $permissions->value[$key];
-
-    if ($test === true) {
-        return true;
-    } elseif ($test !== true && $test !== false) {
-        return $test; // Possibly an int or string limit
-    } else {
-        return false;
-    }
-}
-
 }
