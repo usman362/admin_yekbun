7809a399fb56b9e6031a1918baed086a0cbc2261
Asim Riaz <asimriazasim107@gmail.com>
2025-07-02 13:02:29 +0500

    addd

--- resources/views/content/clips/manage_video.blade.php
+++ resources/views/content/clips/manage_video.blade.php
@@ -28,7 +28,7 @@
         right: 0;
         bottom: 0;
         background: rgba(0, 0, 0, 0.5);
-        color: white;
+        color: rgb(131, 123, 123);
         z-index: 2;
         pointer-events: none;
     }
