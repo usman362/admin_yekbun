4bd9dcb564e3ca1635b9e23556a11c18d3f36b10
Muhammad Usman <120487584+alidevelopement012@users.noreply.github.com>
2025-07-09 04:45:44 +0500

    channges

--- app/Http/Controllers/Api/NotificationsController.php
+++ app/Http/Controllers/Api/NotificationsController.php
@@ -20,8 +20,8 @@ class NotificationsController extends Controller
      */
     public function index()
     {
-        $notifications = NotificationCenter::with('user')->where('is_read', 0)->where('user_id', Auth::id())->get();
-        return ResponseHelper::sendResponse($notifications, 'History has been Fetch Successfully!');
+        $notifications = NotificationCenter::with('user')->where('user_id', Auth::id())->get();
+        return ResponseHelper::sendResponse($notifications, 'Notifications has been Fetch Successfully!');
     }
 
     public function store(Request $request)
