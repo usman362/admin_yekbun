0de5df05cb1ddd5cd6dbd4b5d6ddb1161e623192
Muhammad Usman <120487584+alidevelopement012@users.noreply.github.com>
2025-07-16 03:39:08 +0500

    changes

--- routes/api.php
+++ routes/api.php
@@ -222,6 +222,7 @@ Route::post("/admin-activity/surveys", [AdminActivityController::class, 'store_s
 Route::post("/admin-activity/greetings", [AdminActivityController::class, 'store_greetings']);
 
 Route::get("/admin-activity/get-feeds", [AdminActivityController::class, 'getpopFeeds']);
+Route::get("/admin-activity/get-public-feeds", [AdminActivityController::class, 'getpublicpopFeeds']);
 Route::post("/admin-activity/delete-feeds", [AdminActivityController::class, 'delete_pops']);
 
 Route::get('countries', [CountryController::class, 'index'])->name('countries.index');
