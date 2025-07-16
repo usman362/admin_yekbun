0220cdadc0eec267c1a087096ab6954abbc7da09
Muhammad Usman <120487584+alidevelopement012@users.noreply.github.com>
2025-07-16 21:40:24 +0500

    changes

--- app/Http/Controllers/Api/ViewsController.php
+++ app/Http/Controllers/Api/ViewsController.php
@@ -16,7 +16,7 @@ class ViewsController extends Controller
 {
     public function get_feeds_views(Request $request)
     {
-        $feeds = FeedViews::where('feed_type', $request->feed_type)->where('feed_id', $request->feed_id)->get();
+        $feeds = FeedViews::with('user')->where('feed_type', $request->feed_type)->where('feed_id', $request->feed_id)->get();
         return ResponseHelper::sendResponse($feeds, 'Feeds View Fetched');
     }
     public function store_feeds_views(Request $request)
@@ -34,7 +34,7 @@ class ViewsController extends Controller
 
     public function get_multimedia_views(Request $request)
     {
-        $multimedia = FeedViews::where('media_type', $request->media_type)->where('media_id', $request->media_id)->get();
+        $multimedia = FeedViews::with('user')->where('media_type', $request->media_type)->where('media_id', $request->media_id)->get();
         return ResponseHelper::sendResponse($multimedia, 'Multimedia View Fetched');
     }
 
