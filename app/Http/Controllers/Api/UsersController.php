4850ebebb92ca65f0491b07fbbae34833d8b0611
Muhammad Usman <120487584+alidevelopement012@users.noreply.github.com>
2025-07-17 03:13:45 +0500

    changes

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -31,8 +31,8 @@ class UsersController extends Controller
         $users = User::select('id', 'user_id', 'username', 'image', 'is_online')
             ->where('_id', '!=', Auth::id())
             ->where('is_admin_user', 0)
-            ->whereHas('friends')
-            ->whereHas('family')
+            ->doesntHave('friends')
+            ->doesntHave('family')
             ->get();
         return ResponseHelper::sendResponse($users, 'User Fetch Successfully');
     }
