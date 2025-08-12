a416b7d8856298413bf13e363842bd8bbef462b1
Muhammad Usman <120487584+alidevelopement012@users.noreply.github.com>
2025-08-12 20:48:21 +0500

    changes

--- app/Http/Controllers/Api/ClipsController.php
+++ app/Http/Controllers/Api/ClipsController.php
@@ -58,7 +58,7 @@ class ClipsController extends Controller
         $clip->text_properties = $request->text_properties;
         $videoPath = $request->video;
         $audioPath = storage_path('app/public/' . $request->audio);
-        if (empty($request->audio)) {
+        if (Storage::exists($audioPath)) {
             $audioPath = public_path('audios/empty.mp3');
         }
         $outputPath = storage_path('app/public/videos/clip_' . $uid . '.mp4');
