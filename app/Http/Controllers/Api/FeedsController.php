b9f3b7ea55f1ead242c60594cbcca1c53f6b7b27
Muhammad Usman <usmanshoaib362@gmail.com>
2025-10-14 21:09:28 +0500

    cchanges

--- app/Http/Controllers/Api/FeedsController.php
+++ app/Http/Controllers/Api/FeedsController.php
@@ -40,7 +40,13 @@ class FeedsController extends Controller
     {
 
         // Get authenticated user's latest feed
-        $feedsQuery = Feed::with(['user', 'shareUser','parentFeed'])
+        $feedsQuery = Feed::with(['user', 'shareUser','parentFeed' => function($pf){
+            $pf->comments_count = 0;
+            $pf->voice_comments_count = 0;
+            $pf->likes_count = 0;
+            $pf->views_count = 0;
+            $pf->shares_count = 0;
+        }])
             ->orderBy('created_at', 'desc');
 
         if (!empty($request->user_id)) {
