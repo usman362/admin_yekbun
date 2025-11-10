4edee7b115aae8ec9fb36773aa5cad1efb2d7e38
Muhammad Usman <usmanshoaib362@gmail.com>
2025-11-10 18:23:07 +0500

    changes

--- resources/views/layouts/sections/menu/verticalMenu.blade.php
+++ resources/views/layouts/sections/menu/verticalMenu.blade.php
@@ -1233,7 +1233,7 @@
         </li>
 
         <li
-            class="menu-item {{ Request::is('zarok-stories', 'zarok-videos', 'zarok-movies', 'zarok-series','tv-api-list') ? 'active open' : '' }}">
+            class="menu-item {{ Request::is('zarok-stories', 'zarok-videos', 'zarok-movies', 'zarok-series') ? 'active open' : '' }}">
             <a href="javascript:void(0);" class="menu-link menu-toggle">
                 <i class="menu-icon tf-icons bx bx-tv"></i>
                 <div>Zarok TV</div>
@@ -1257,11 +1257,6 @@
                 <li class="menu-item {{ Request::is('zarok-series') ? 'active' : '' }}">
                     <a href="{{ route('zarok.series') }}" class="menu-link">
                         <div>Add Series</div>
-                    </a>
-                </li>
-                <li class="menu-item {{ Request::is('tv-api-list') ? 'active' : '' }}">
-                    <a href="{{ route('tv.api.list') }}" class="menu-link">
-                        <div>API List</div>
                     </a>
                 </li>
             </ul>
@@ -1289,6 +1284,13 @@
                     </a>
                 </li>
             </ul>
+        </li>
+
+        <li class="menu-item {{ Request::is('tv-api-list') ? 'active' : '' }}">
+            <a href="{{ url('/tv-api-list') }}" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-tv"></i>
+                <div>Yekbun TV API</div>
+            </a>
         </li>
 
         <li class="menu-item">
