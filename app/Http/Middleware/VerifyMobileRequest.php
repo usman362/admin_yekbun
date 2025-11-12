99d1d68264980c03620ce64303e74cf913837de1
Muhammad Usman <usmanshoaib362@gmail.com>
2025-11-12 14:39:58 +0500

    changes

--- app/Http/Middleware/VerifyMobileRequest.php
+++ app/Http/Middleware/VerifyMobileRequest.php
@@ -27,7 +27,7 @@ class VerifyMobileRequest
         }
 
         // Allow only known mobile UA keywords
-        $allowedAgents = ['okhttp', 'android', 'cfnetwork', 'iphone', 'ios', 'flutter', 'reactnative', 'mobile'];
+        $allowedAgents = ['okhttp', 'android', 'postman','cfnetwork', 'iphone', 'ios', 'flutter', 'reactnative', 'mobile'];
 
         $isMobile = false;
         foreach ($allowedAgents as $keyword) {
