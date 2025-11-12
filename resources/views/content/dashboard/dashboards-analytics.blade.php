d83b16ea4f8f840856ff7365945cc2694535024d
Muhammad Usman <usmanshoaib362@gmail.com>
2025-11-12 15:28:37 +0500

    chages

--- resources/views/content/dashboard/dashboards-analytics.blade.php
+++ resources/views/content/dashboard/dashboards-analytics.blade.php
@@ -469,7 +469,7 @@
                             <h4>System Settings</h4>
                         </div>
 
-                        <div class="settings-list">
+                        {{-- <div class="settings-list">
                             <div class="setting-item">
                                 <div class="setting-icon">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25"
@@ -569,7 +569,7 @@
                                     <label for="login"></label>
                                 </div>
                             </div>
-                        </div>
+                        </div> --}}
                     </div>
                 </div>
 
