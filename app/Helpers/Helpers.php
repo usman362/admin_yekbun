44501fd9d8b5df03470bda3548b6144529306f60
Muhammad Usman <alidevelopement01@gmail.com>
2025-12-02 15:04:57 +0500

    video conversion mostly working now

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -323,7 +323,8 @@ class Helpers
     // Convert MP4 (H.264) → MP4 (H.265 HEVC)
     public static function convertToH265($inputPath, $outputPath)
     {
-        $cmd = "ffmpeg -i {$inputPath} -c:v libx265 -crf 28 -preset fast -c:a aac {$outputPath} -y";
+        // $cmd = "ffmpeg -i {$inputPath} -c:v libx265 -crf 28 -preset fast -c:a aac {$outputPath} -y";
+        $cmd = "ffmpeg -i {$inputPath} -vcodec libx264 -crf 28 -preset veryslow -acodec aac {$outputPath}";
         exec($cmd, $output, $returnCode);
         return $returnCode === 0;
     }
