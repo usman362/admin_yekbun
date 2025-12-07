1cc5dd3d7ec294de4765de5b917dc9e80820d7c2
Muhammad Usman <alidevelopement01@gmail.com>
2025-12-08 01:31:15 +0500

    deactivated users delete by cron

---
+++ app/Console/Commands/DeleteDeactivatedUsers.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\User;
+use Carbon\Carbon;
+
+class DeleteDeactivatedUsers extends Command
+{
+    protected $signature = 'users:delete-deactivated';
+    protected $description = 'Delete users who have been deactivated for more than 90 days';
+
+    public function handle()
+    {
+        $date = Carbon::now()->subDays(90);
+
+        $users = User::where('status', 0)
+                    ->where('deactivated_at', '<', $date)
+                    ->get();
+
+        foreach ($users as $user) {
+            $user->delete();
+        }
+
+        $this->info('Deleted users deactivated more than 90 days ago.');
+    }
+}
