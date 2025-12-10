512089b1967285d47ff7987bf2843ee7973ff8bc
Muhammad Usman <alidevelopement01@gmail.com>
2025-12-11 03:14:22 +0500

    changes

--- routes/api.php
+++ routes/api.php
@@ -79,7 +79,7 @@ use App\Http\Controllers\Api\UserSuggestionController;
 |
 */
 
-// Route::middleware(['verify.mobile'])->group(function () {
+Route::middleware(['verify.mobile'])->group(function () {
 
     Route::get('/getfeeds', [AvatarsController::class, 'getfeeds']);
     Route::post('/postfeed', [AvatarsController::class, 'postfeed']);
@@ -580,4 +580,4 @@ use App\Http\Controllers\Api\UserSuggestionController;
     });
     // });
 
-// });
+});
