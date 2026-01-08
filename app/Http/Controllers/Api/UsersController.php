ee312ca641fec7220bbae4ef96f7933381d83d35
usman362 <usmanshoaib362@gmail.com>
2026-01-08 16:08:47 +0500

    changes

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -36,16 +36,24 @@ class UsersController extends Controller
     {
         $authId = Auth::id();
 
+        // Get all related user IDs (friends + family)
+        $excludedUserIds = UserFriends::where('user_id', $authId)
+            ->orWhere('friend_id', $authId)
+            ->whereIn('user_type', ['friends', 'family'])
+            ->get()
+            ->flatMap(function ($row) use ($authId) {
+                return $row->user_id === $authId
+                    ? [$row->friend_id]
+                    : [$row->user_id];
+            })
+            ->unique()
+            ->values()
+            ->toArray();
+
         $users = User::select('_id', 'user_id', 'username', 'image', 'is_online')
             ->where('_id', '!=', $authId)
             ->where('is_admin_user', 0)
-            ->whereDoesntHave('relations', function ($q) use ($authId) {
-                $q->whereIn('user_type', ['friends', 'family'])
-                ->where(function ($q) use ($authId) {
-                    $q->where('user_id', $authId)
-                        ->orWhere('friend_id', $authId);
-                });
-            })
+            ->whereNotIn('_id', $excludedUserIds)
             ->get();
 
         return ResponseHelper::sendResponse($users, 'User Fetch Successfully');
