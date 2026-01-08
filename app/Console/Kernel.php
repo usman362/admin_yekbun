34aa7e9667dd743798f33e2848791ecfd58abfa8
usman362 <usmanshoaib362@gmail.com>
2026-01-08 15:37:12 +0500

    command added for downgrade trial user

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,6 +17,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command('users:delete-deactivated')->daily();
+        $schedule->command('users:downgrade-trial')->hourly();
     }
 
     /**
