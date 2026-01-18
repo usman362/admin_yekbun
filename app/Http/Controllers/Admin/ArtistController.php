5155972e70b78bb8ba8233c25f74f7ad45811c91
Muhammad Usman <alidevelopement01@gmail.com>
2026-01-19 03:23:39 +0500

    changes

--- app/Http/Controllers/Admin/ArtistController.php
+++ app/Http/Controllers/Admin/ArtistController.php
@@ -108,8 +108,8 @@ class ArtistController extends Controller
                     return '<a href="javascript:void(0)" class="text-black artistDetail" data-id="' . $artist->id . '" data-section="videos" data-bs-toggle="modal"
                                 data-name="' . $artist->name . '" data-image="' . env('BUNNY_CDN_URL') . $artist->image . '"
                                 data-gender="' . $artist->gender . '"
-                                data-province=""
-                                data-bs-target="#artistDetailModal">' . $artist->videos->count() . '</a>';
+                                data-province="' . ($artist->province->name ?? 'N/A') . '"
+                                data-bs-target="#artistDetailModal">0</a>';
                 })
                 ->addColumn('like', function () {
                     return '0';
