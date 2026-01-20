1112d4eb0aef68dcf90f233b35c8f7b856422b37
Muhammad Usman <alidevelopement01@gmail.com>
2026-01-21 00:03:50 +0500

    chages

--- app/Http/Controllers/Api/VotingController.php
+++ app/Http/Controllers/Api/VotingController.php
@@ -68,7 +68,7 @@ class VotingController extends Controller
             //         // Case 2: no reactions at all
             //          ->orWhereDoesntHave('reactions');
             // })
-            ->orderBy('created_at', 'desc')->limit(1);
+            ->with('reactions')->orderBy('created_at', 'desc')->get();
         return ResponseHelper::sendResponse($votings, 'Votings Fetch Successfully!');
     }
 
