16102cc891661fcc6b388390477c3e30d1ee4925
Muhammad Usman <alidevelopement01@gmail.com>
2026-02-21 16:37:20 +0500

    50 to 60 percent reduce ffmpeg

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -388,20 +388,47 @@ class Helpers
     public static function convertToH265($inputPath, $outputPath)
     {
         // $cmd = "ffmpeg -i {$inputPath} -c:v libx265 -crf 28 -preset fast -c:a aac {$outputPath} -y";
+
+        //Without Android
         // $cmd = "ffmpeg -i {$inputPath} -vcodec libx264 -crf 28 -preset fast -acodec aac {$outputPath}";
+
+        //40% Reduce
+        // $cmd = "ffmpeg -y -i {$inputPath} \
+        //         -c:v libx264 \
+        //         -profile:v main \
+        //         -level 3.1 \
+        //         -pix_fmt yuv420p \
+        //         -crf 28 \
+        //         -preset fast \
+        //         -movflags +faststart \
+        //         -c:a aac \
+        //         -b:a 96k \
+        //         -ar 44100 \
+        //         -ac 2 \
+        //         -f mp4 \
+        //         {$outputPath} 2>&1";
+
+        //50 to 55% Reduce
+        // $cmd = "ffmpeg -y -i {$inputPath} \
+        //         -map 0:v:0 -map 0:a? \
+        //         -c:v libx264 \
+        //         -pix_fmt yuv420p \
+        //         -crf 30 \
+        //         -preset fast \
+        //         -movflags +faststart \
+        //         -c:a aac \
+        //         -b:a 64k \
+        //         -ar 44100 \
+        //         -ac 2 \
+        //         -f mp4 \
+        //         {$outputPath} 2>&1";
+
+        // 55% to 60% Reduce
         $cmd = "ffmpeg -y -i {$inputPath} \
-                -c:v libx264 \
-                -profile:v baseline \
-                -level 3.0 \
-                -pix_fmt yuv420p \
-                -crf 28 \
-                -preset fast \
-                -movflags +faststart \
-                -c:a aac \
-                -b:a 96k \
-                -ar 44100 \
-                -ac 2 \
-                {$outputPath}";
+        -map 0:v:0 -map 0:a? \
+        -c:v libx264 -pix_fmt yuv420p -crf 32 -preset fast -movflags +faststart \
+        -c:a aac -b:a 64k -ar 44100 -ac 2 \
+        -f mp4 {$outputPath} 2>&1";
         exec($cmd, $output, $returnCode);
         return $returnCode === 0;
     }
